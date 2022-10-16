7e256e3ef4b050192697dc60be6e78d29fdaf5a3
Your Name <you@example.com>
2022-10-16 17:35:11 +0200

    fixed github pages share link

--- publish.js
+++ publish.js
@@ -1,3 +1,2 @@
 const execSync = require('child_process').execSync;
-const mssg = 'BUILD - ' + process.argv[2];
-execSync('npm run build-only && git add dist -f && git commit -m "Adding dist" &&  git subtree push --prefix dist origin gh-pages');
+execSync('npm run build-only && ncp ./dist/index.html ./dist/404.html && git add dist -f && git commit -m "Adding dist" &&  git subtree push --prefix dist origin gh-pages');
